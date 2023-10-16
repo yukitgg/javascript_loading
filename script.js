902956df3efc96fafc894d7a02af9b81f35ed6fa
yukitgg <107914654+yukitgg@users.noreply.github.com>
2023-10-16 14:24:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// JavaScript
+
+//画面が読み込まれたら animation を呼び出す
+window.addEventListener('load', animation);
+
+// 定数の定義
+const loading = document.getElementById('loading');
+
+// cssクラスを追加する関数
+function animation(){
+  loading.classList.add('loaded');
+}
+//画面が読み込まれたら animation を呼び出す
+//window.addEventListener('load', animation);
+
+window.setTimeout( animation, 2000 );
